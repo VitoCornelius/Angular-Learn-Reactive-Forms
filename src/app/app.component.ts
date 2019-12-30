a39d14e35a6f8359512c78bc55c5e88525257dfb
Rafal ROjek <rafalrojek90@gmail.com>
2019-12-30 13:04:03 +0100

    Create my own custom validator -> bind to this DO retrun null if the input is valid

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,14 +7,18 @@ import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  genders = ['male', 'female'];
+  genders : String[] = ['male', 'female'];
   signupForm : FormGroup;
+
+  forbiddenUsernames : String[] = ['Chris', 'Anna'];
+  
 
   ngOnInit() {
     //should initialize before rendering
     this.signupForm = new FormGroup({
       'userData' : new FormGroup({
-        'username' : new FormControl(null, Validators.required),  //we can set the default value, validators, async validators  
+        'username' : new FormControl(null, [Validators.required, this.forbiddenNames.bind(this)]), //have to bind to THIS class 
+        //we can set the default value, validators, async validators  
         'email' : new FormControl(null, [Validators.required, Validators.email]),
       }),
       'gender' : new FormControl('male'),
@@ -30,5 +34,13 @@ export class AppComponent implements OnInit {
     const control = new FormControl(null, Validators.required);
     (<FormArray>this.signupForm.get('hobbies')).push(control);
   }
+
+  // {nameIsForbidden : true} -> this returns something like this 
+  forbiddenNames(control : FormControl) : {[s: string]:boolean} {
+    if (this.forbiddenUsernames.indexOf(control.value) !== -1) {
+      return {'nameIsForbidden' : true};
+    }
+    return null; // IF THE VALIDATION IS SUCCESSFUL, YOU SHOULD PASS NULL !!!!!!!!!!!
+  } 
 
 }
